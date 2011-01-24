7c1c5f7ba20d33232a4fc1825f6d84765873a864
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-01-24 12:34:59 +1100

    Initial commit to laptop-death-tally-dashboard.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'laptop-death-tally-dashboard'
+
+class Test::Unit::TestCase
+end
